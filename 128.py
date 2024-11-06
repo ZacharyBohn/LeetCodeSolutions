bb2e2b17862de7debeab7e6a0bb261744d2f5753
Zachary Bohn <zacharybohn95@gmail.com>
2024-11-06 08:11:59 -0500

    3rd solution to 128

--- 128.py
+++ 128.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# the first solution that I came up with
 class Solution1:
 	def longestConsecutive(self, nums: List[int]) -> int:
 		hashset = set(nums)
@@ -13,7 +14,7 @@ class Solution1:
 			longest_seq = max(longest_seq, current_seq)
 		return longest_seq
 
-
+# the second and best solution I came up with
 class Solution2:
     def longestConsecutive(self, nums: List[int]) -> int:
         hashset = {}
@@ -28,4 +29,31 @@ class Solution2:
             hashset[start] = [start, end]
             hashset[end] = [start, end]
             longest = max(longest, end - start + 1)
+        return longest
+
+# another version of the second solution that I did
+# just because
+#
+# in the hashset is stored n, start, end as
+# {n*2: start, n*2+1: end}
+# this just takes advantage of the fact that I am storing
+# 2 values for each number, the currently known start
+# and the currently known end
+class Solution3:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        hashset = {}
+        longest = 0
+        for n in nums:
+            n2 = n*2
+            if n2 in hashset: continue
+            start, end = n2, n2
+            if n2-2 in hashset: start = hashset[n2-2]
+            if n2+2 in hashset: end = hashset[n2+3]
+            # to be able to skip duplicates of n
+            hashset[n2] = None
+            hashset[start] = start
+            hashset[start+1] = end
+            hashset[end] = start
+            hashset[end+1] = end
+            longest = max(longest, (end // 2) - (start//2) + 1)
         return longest
